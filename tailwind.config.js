dd5dd55c61b8c93e23408b45d942e4d42e20ef74
JosimarAkpomudia <123875493+JosimarAkpomudia@users.noreply.github.com>
2024-03-05 08:39:17 +0100

    Adding some appearance animation
    Adding some appearance animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,35 @@ export default {
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+      },      
+      animation: {
+        fade: 'fadeIn 2s ease-in forwards',
+        slideIn: 'slideIn 2s ease-in backwards',
+        slideOut: 'slideOut 2s ease-out forwards',
+        slideOut2: 'slideOut 3s ease-out forwards',
+        slideOut3: 'slideOut 4s ease-out forwards',
+        infiniteScroll: 'infiniteScroll 25s linear infinite',
       },
+
+      // that is actual animation
+      keyframes: theme => ({
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 100 },
+        },
+        slideIn: {
+          from: { transform: 'translateX(100%)', opacity: 0 },
+          to: { transform: 'translateX(0%)', opacity: 100 },
+        },
+        slideOut: {
+          from: { transform: 'translateX(100%)', opacity: 0},
+          to: { transform: 'translateX(0)', opacity: 100 },
+        },
+        infiniteScroll:{
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }),
     },
   },
   plugins: [
